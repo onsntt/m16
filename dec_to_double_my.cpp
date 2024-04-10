9964ecb457ec7e45827f63a8297a1e53adb1f290
onsntt <onsnt@mail.ru>
2024-04-10 14:54:56 +0500

    double <--> decimal

---
+++ dec_to_double_my.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+int main() {
+    int decimal;
+    while(true)
+    {
+      std::cout << "Enter your decimal value: ";
+      std::cin >>decimal;
+         if (decimal == -1) return 0;
+           else
+             {
+                std::vector<int> vec(0);
+                int result;
+                    do {
+                         result = decimal%2;
+                         vec.push_back(result);
+                         decimal = decimal/2;
+                       }
+                        while (decimal > 0);
+
+
+for (int i = vec.size() -1; i >= 0; i--)
+{
+    std::cout << vec[i] << " ";
+}
+std::cout << "\n";
+            }
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ double_to_decimal_my.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+
+int main() {
+while (true)
+{
+std::cout << "Enter your double value ";
+
+std::string doubleString = "";
+std::cin >> doubleString;
+//if (doubleString == '-1') return 0;
+
+int sum = 0;
+
+
+std::vector<bool>vec(doubleString.length());
+for (int i = 0; i < doubleString.length(); i++)
+{
+ if (doubleString[i] == '1' )
+ vec[i] = true;
+ else if (doubleString[i] == '0')  vec[i] = false;
+ else
+ {
+     std::cout << "Incorrect input \n";
+     return 0;
+ } 
+}
+
+for (int i = 0; i<vec.size(); i++)
+{
+   if (vec[i] == true)
+   {
+    sum += pow(2,(vec.size()-1 - i));
+   }
+
+}
+std::cout << sum << "\n";
+
+}
+}
